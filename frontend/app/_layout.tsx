003ca2625cfb706b8596408f9003c03e65056c74
lolerwastaken <waritsuntrarom@gmail.com>
2026-05-13 23:38:39 +0700

    Added home icon to top left corner of every page

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
+import { Text, View, TouchableOpacity, Image } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => router.push("/")} style={{ marginLeft: 15 }}>
+            <Image 
+              source={require("../assets/images/message_logo.png")}
+              style={{ width: 40, height: 40 }}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        ),
+      }}
+    />
+  );
 }
